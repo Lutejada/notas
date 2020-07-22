46a7ef32a29c5248749db73d41bb32b78e719a7f
Luis Tejada <luetejava@gmail.com>
2020-07-21 19:20:12 -0500

    inicio proyecto notas

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+
+//inicializaciones
+const app = express()
+
+//setings
+app.set('port', process.env.port || 4000);
+app.set('views', path.join(__dirname, 'views')); //para enrutar la carpeta de views porque esta dentro de src
+
+//middlewares
+app.use(express.urlencoded({ extended: false }))
+
+//variables globales
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send('hello word')
+})
+
+//statics 
+
+
+
+
+
+
+
+module.exports = app
